fb3a671feb6505722232dbff49d3427964b6d433
jayabalan <jbalachandran@wayfair.com>
2019-03-31 19:19:02 -0400

    Fixing commands

--- modules/consul_template/manifests/init.pp
+++ modules/consul_template/manifests/init.pp
@@ -2,15 +2,15 @@ class consul_template (
   $consul_version          = 'consul_1.4.4_linux_amd64.zip',
   $consul_template_version = 'consul-template_0.20.0_darwin_386.tgz'
 ){
-  package { ['wget','uzip']:
-    ensure => installed,
-  }
+  package { 'wget': ensure => 'installed' }
+  package { 'unzip': ensure => 'installed' }
+
   exec { 'install consul':
     command => "wget https://releases.hashicorp.com/consul/1.4.4/${consul_version} && tar -xvf ${consul_version}",
     path    => '/usr/local/bin'
   }
   exec { 'install_consul_template':
-    command => "wget /usr/local/bin/https://releases.hashicorp.com/consul-template/0.20.0/${consul_template_version} && uzip ${consul_template_version}",
+    command => "wget /usr/local/bin/https://releases.hashicorp.com/consul-template/0.20.0/${consul_template_version} && unzip ${consul_template_version}",
     path    => '/usr/local/bin'
   }
 }
